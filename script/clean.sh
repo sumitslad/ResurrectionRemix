hagar006/Validus

Create clean and hard reset script

script/clean.sh
@@ -1,0 +1,7 @@
+#!/bin/bash
+
+export PATH=/data/bin:$PATH
+
+cd /data/jenkins/workspace/ValidusRom
+
+repo forall -vc "git reset --hard" && repo forall -vc "git clean -df"

sumitslad/ResurrectionRemix

script/clean.sh
@@ -1,0 +1,7 @@
+#!/bin/bash
+
+export PATH=/data/bin:$PATH
+
+cd /data/jenkins/workspace/ResurrectionRemix
+
+repo forall -vc "git reset --hard" && repo forall -vc "git clean -df"